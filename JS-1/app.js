c0ddd42ce91c1409dc06cdd95ec47d4a264f2862
Murali <muralivvrsn75683@gmail.com>
2022-11-26 16:38:20 +0530

    clock created

--- JS-1/app.js
+++ JS-1/app.js
@@ -12,11 +12,19 @@ const obj = {
 };
 console.log(letters[0].innerText);
 letters.forEach((element) => {
-  element.onclick = function () {
+  element.addEventListener("click", function () {
+    element.removeAttribute("style");
+    console.log(element);
+    document.getElementById(`${element.innerText}`).style.animation =
+      "clickMe 0.2s";
     var audio = new Audio(obj[element.innerText.toLowerCase()]);
     audio.play();
-  };
+  });
   element.addEventListener("keypress", function (e) {
+    document.getElementById(`${e.key.toUpperCase()}`).removeAttribute("style");
+
+    document.getElementById(`${e.key.toUpperCase()}`).style.animation =
+      "clickMe 0.2s";
     var audio = new Audio(obj[e.key]);
     audio.play();
   });
